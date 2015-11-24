d8942c2baa9ee53342e750dae483083989720384
medfiras <hajjejfiras@gmail.com>
2015-11-24 18:39:34 +0100

    updating accounts template

--- bazinga/urls.py
+++ bazinga/urls.py
@@ -54,7 +54,7 @@ urlpatterns = patterns('',
     # url(r'^(?P<username>[\.\w]+)/edit/$',userena_views.profile_edit, {'template_name': 'userena/profile_form.html'},name='userena_profile_edit'),
     # url(r'^(?P<username>[\@\.\w-]+)/password/$', userena_views.password_change, {'template_name': 'userena/profile_form.html'}, name="userena_password_change"),
     # url(r'^(?P<username>[\@\.\w-]+)/email/$', userena_views.email_change, {'template_name': 'userena/profile_form.html'}, name='userena_email_change'),
-    # url(r'^(?P<username>[\.\w-]+)/edit/$','userena.views.profile_edit',{'edit_profile_form': CustomEditProfileForm,'template_name': 'userena/profile_form.html'},name='userena_profile_edit'),
+    url(r'^(?P<username>[\.\w-]+)/edit/$','userena.views.profile_edit',{'edit_profile_form': CustomEditProfileForm,'template_name': 'userena/profile_form.html'},name='userena_profile_edit'),
 
     url(r'^(?P<username>[\@\.\w-]+)/password/complete/$', userena_views.direct_to_user_template, {'template_name': 'userena/profile_detail.html'}, name='userena_password_change_complete'),
     # url(r'^(?P<username>[\@\.\w-]+)/password/$', userena_views.direct_to_user_template, {'template_name': 'userena/profile_detail.html'}, name='userena_password_change'),
